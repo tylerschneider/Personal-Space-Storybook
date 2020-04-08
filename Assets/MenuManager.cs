8d3c799f912ca89ef6d35dfbeed6a76b95b18744
tylerschneider <46699128+tylerschneider@users.noreply.github.com>
2020-04-08 16:03:42 -0600

    Started Menus

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] menus;
+    public GameObject lastMenu;
+    public static MenuManager Instance;
+
+    private void Start()
+    {
+        if(!Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void ChangeMenu(GameObject selectedMenu)
+    {
+        foreach (GameObject menu in menus)
+        {
+            if(menu != selectedMenu && menu.activeSelf)
+            {
+                menu.SetActive(false);
+                lastMenu = menu;
+            }
+            else if(menu == selectedMenu)
+            {
+                menu.SetActive(true);
+            }
+        }
+    }
+
+    public void BackButton()
+    {
+        ChangeMenu(lastMenu);
+    }
+}
