7d8d015141a8a6d2e96fc8a24548d4585982b579
tylerschneider <46699128+tylerschneider@users.noreply.github.com>
2020-04-08 19:37:43 -0600

    Working PIN
    pin is saved to \AppData\LocalLow\DefaultCompany\Personal Space Storybook\userinfo.save
    
    delete this file if pin needs changed

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject[] menus;
     public GameObject lastMenu;
     public static MenuManager Instance;
+    public GameObject mainMenu;
+    public GameObject createPinScreen;
+    public InputField checkPinInput;
+    public GameObject checkPinError;
+
 
     private void Start()
     {
@@ -14,6 +22,11 @@ public class MenuManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
+        }
+
+        if(GetPin() == 0)
+        {
+            ChangeMenu(createPinScreen);
         }
     }
 
@@ -33,8 +46,59 @@ public class MenuManager : MonoBehaviour
         }
     }
 
+    public void CheckPin(GameObject selectedMenu)
+    {
+        if(GetPin().ToString() == checkPinInput.text)
+        {
+            ChangeMenu(selectedMenu);
+            checkPinError.SetActive(false);
+            checkPinInput.text = "";
+        }
+        else
+        {
+            checkPinError.SetActive(true);
+        }
+    }
+
     public void BackButton()
     {
         ChangeMenu(lastMenu);
+    }
+
+    public void SetPin(GameObject input)
+    {
+        PIN pin = new PIN();
+
+        pin.pinNumber = int.Parse(input.GetComponent<InputField>().text);
+
+        print(Application.persistentDataPath);
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/userdata.save");
+        bf.Serialize(file, pin);
+        file.Close();
+
+        ChangeMenu(mainMenu);
+    }
+
+    public int GetPin()
+    {
+        if (File.Exists(Application.persistentDataPath + "/userdata.save"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/userdata.save", FileMode.Open);
+            PIN pin = (PIN)bf.Deserialize(file);
+            file.Close();
+
+            int pinNumber = pin.pinNumber;
+
+            return pinNumber;
+        }
+        else
+        {
+            Debug.Log("No game saved!");
+
+            return 0;
+        }
     }
 }
