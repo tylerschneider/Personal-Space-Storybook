973339bd3a4c7d29b315254b90dd91e51078ce98
Tavi Kohn <tavi.kohn@gmail.com>
2020-05-06 19:55:25 -0600

    Add AR integration project

---
+++ Assets/Scripts/GuidanceCircle.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidanceCircle : MonoBehaviour
+{
+    // Public fields
+    public Material[] materials;
+    public float strangerDistance = 10;
+    public float friendDistance = 5;
+    public float familyDistance = 3;
+    public GameObject guidanceCircleObject;
+    public SpriteRenderer guidanceCircleSprite;
+    public Sprite[] sprites;
+
+    // Private fields
+    private GameObject player;
+    private float distanceToPlayer;
+    private enum Classification
+    {
+        Stranger, Friend, Family, None
+    }
+    private Classification currentClassification;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find player object in scene
+        /* foreach (var go in this.gameObject.scene.GetRootGameObjects())
+        {
+            if (go.tag == "MainCamera") {
+                player = go;
+            }
+        } */
+        player = Camera.main.gameObject;
+        currentClassification = Classification.None;
+        //InvokeRepeating("classifyDistance", 1f, 1f);
+        guidanceCircleSprite = guidanceCircleObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate distance to player
+        distanceToPlayer = Vector3.Distance(this.transform.position, player.transform.position);
+        Debug.Log("Distance to player: " + distanceToPlayer);
+
+        // Check distance against settings
+        classifyDistance();
+    }
+
+    private void classifyDistance()
+    {
+        if (distanceToPlayer < strangerDistance && distanceToPlayer > friendDistance)
+        {
+            if (currentClassification != Classification.Stranger)
+            {
+                GetComponent<Renderer>().material = materials[0];
+                guidanceCircleSprite.sprite = sprites[0];
+                currentClassification = Classification.Stranger;
+                Debug.Log("In stranger distance");
+            }
+        }
+        else if (distanceToPlayer < friendDistance && distanceToPlayer > familyDistance)
+        {
+            if (currentClassification != Classification.Friend)
+            {
+                GetComponent<Renderer>().material = materials[1];
+                guidanceCircleSprite.sprite = sprites[1];
+                currentClassification = Classification.Friend;
+                Debug.Log("In family distance");
+            }
+        }
+        else if (distanceToPlayer < familyDistance)
+        {
+            if (currentClassification != Classification.Family)
+            {
+                GetComponent<Renderer>().material = materials[2];
+                guidanceCircleSprite.sprite = sprites[2];
+                currentClassification = Classification.Family;
+                Debug.Log("In friend distance");
+            }
+        } else if (distanceToPlayer > strangerDistance) {
+            GetComponent<Renderer>().material = materials[3];
+            guidanceCircleSprite.sprite = sprites[3];
+                currentClassification = Classification.None;
+                Debug.Log("Far away");
+        }
+    }
+}
---
+++ Assets/Scripts/ScenePlacer.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ScenePlacer : MonoBehaviour
+{
+    [SerializeField]
+    private ARPlaneManager planeManager;
+    [SerializeField]
+    private ARPointCloudManager pointCloudManager;
+    [SerializeField]
+    private ARRaycastManager raycastManager;
+    [SerializeField]
+    private ARAnchorManager anchorManager;
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private Material previewMaterial;
+
+    private GameObject previewObject = null;
+    private GameObject placedObject = null;
+
+    private enum PlacerState
+    {
+        Ready,
+        Placing,
+        Placed,
+    }
+
+    private PlacerState state = PlacerState.Ready;
+
+    void OnEnable()
+    {
+        // Hide newly added planes, unless we are in placement mode
+        planeManager.planesChanged += OnPlaneChange;
+    }
+
+    void OnDisable()
+    {
+        planeManager.planesChanged -= OnPlaneChange;
+    }
+
+    void OnPlaneChange(ARPlanesChangedEventArgs changes)
+    {
+        if (state != PlacerState.Placing && changes.added.Count > 0)
+        {
+            foreach (var plane in planeManager.trackables)
+            {
+                plane.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void Update()
+    {
+        // TODO allow dragging to rotate
+        if (state == PlacerState.Placing)
+        {
+            var previewHits = new List<ARRaycastHit>();
+            raycastManager.Raycast(new Ray(Camera.main.transform.position, Camera.main.transform.forward), previewHits, TrackableType.Planes);
+            if (previewHits.Count > 0)
+            {
+                var pose = previewHits[0].pose;
+                if (previewObject == null)
+                {
+                    previewObject = Instantiate(prefab);
+                    foreach (var renderer in previewObject.GetComponentsInChildren<Renderer>())
+                    {
+                        var materials = new Material[renderer.materials.Length];
+                        for (var i = 0; i < materials.Length; i++)
+                        {
+                            materials[i] = previewMaterial;
+                        }
+                        renderer.materials = materials;
+                    }
+                }
+                previewObject.SetActive(true);
+                previewObject.transform.position = pose.position;
+                previewObject.transform.rotation = pose.rotation;
+                previewObject.transform.forward = -previewObject.transform.forward;
+            }
+            else
+            {
+                if (previewObject != null)
+                {
+                    previewObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void OnButton(TMPro.TextMeshProUGUI buttonText)
+    {
+        switch (state)
+        {
+            case PlacerState.Ready:     // "Place Object" clicked
+                state = PlacerState.Placing;
+                buttonText.text = "Place";
+                planeManager.enabled = true;
+                pointCloudManager.enabled = true;
+                foreach (var plane in planeManager.trackables)
+                {
+                    plane.gameObject.SetActive(true);
+                }
+                foreach (var cloud in pointCloudManager.trackables)
+                {
+                    cloud.gameObject.SetActive(true);
+                }
+                break;
+            case PlacerState.Placing:   // "Place" clicked
+                var placeHits = new List<ARRaycastHit>();
+                raycastManager.Raycast(new Ray(Camera.main.gameObject.transform.position, Camera.main.gameObject.transform.forward), placeHits, TrackableType.Planes);
+                if (placeHits.Count > 0)
+                {
+                    Destroy(previewObject);
+                    previewObject = null;
+                    planeManager.enabled = false;
+                    pointCloudManager.enabled = false;
+                    foreach (var plane in planeManager.trackables)
+                    {
+                        plane.gameObject.SetActive(false);
+                    }
+                    foreach (var cloud in pointCloudManager.trackables)
+                    {
+                        cloud.gameObject.SetActive(false);
+                    }
+                    var pose = placeHits[0].pose;
+                    placedObject = Instantiate(prefab, pose.position, pose.rotation);
+                    var anchorComponent = placedObject.AddComponent<ARAnchor>();
+                    anchorComponent = anchorManager.AddAnchor(pose);
+                    placedObject.transform.forward = -placedObject.transform.forward;
+                    // DontDestroyOnLoad(placedObject);
+                    state = PlacerState.Placed;
+                    buttonText.text = "Remove Object";
+                }
+                break;
+            case PlacerState.Placed:    // "Remove Object" clicked
+                state = PlacerState.Ready;
+                buttonText.text = "Place Object";
+                Destroy(placedObject);
+                break;
+        }
+    }
+}
